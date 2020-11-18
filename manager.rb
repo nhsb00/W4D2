1855a17ddd2e833f6df3036c9f809be9a7b0bd75
soobin <nhsb00@hotmail.com>
2020-11-18 10:54:10 -0500

    start error handling

--- manager.rb
+++ manager.rb
@@ -14,12 +14,12 @@ class Manager < Employee
         # bonus = (total salary of all sub-employees) * multiplier
         sum_salary * multiplier
     end
-
+    protected
     def sum_salary 
         salaries = 0
         self.employees.each do |emp|
             if emp.is_a?(Manager)
-                salaries = salaries + emp.sum_salary + emp.salary
+                salaries = salaries + emp.salary + emp.sum_salary # if it is private we cannot call emp.sum_salary
             else
                 salaries = salaries + emp.salary
             end
